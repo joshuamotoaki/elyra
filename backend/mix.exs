d1d15c6ade7fc1f7dd8e1f383142f762812ef6ac
Joshua Lau <dachshunduniverse@gmail.com>
2025-12-05 22:04:20 -0500

    Get rid of i18n

--- backend/mix.exs
+++ backend/mix.exs
@@ -39,7 +39,6 @@ defmodule Backend.MixProject do
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
