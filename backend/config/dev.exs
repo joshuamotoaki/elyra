e061edcb29628f091c5377f26f83b664b36e1c54
Joshua Lau <dachshunduniverse@gmail.com>
2025-12-05 22:14:06 -0500

    Fix docker network issues

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -11,10 +11,17 @@ config :backend, Backend.Repo,
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
+# When PHX_IP=0.0.0.0 (e.g. in Docker), bind to all interfaces
+phx_ip =
+  case System.get_env("PHX_IP") do
+    "0.0.0.0" -> {0, 0, 0, 0}
+    _ -> {127, 0, 0, 1}
+  end
+
 config :backend, BackendWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  # Set PHX_IP=0.0.0.0 to allow access from other machines (e.g. Docker).
+  http: [ip: phx_ip, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
